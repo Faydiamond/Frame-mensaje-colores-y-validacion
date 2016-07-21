8e02fc27b997aa1bbd71e4ef0a6c17dac106d033
Faydiamond <faba_1512@hotmail.com>
2016-07-21 08:08:06 -0500

    source

---
+++ src/mensaje/Mensaje.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mensaje;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author usuario
+ */
+public class Mensaje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "open package Formularios and run Mens.java");
+    }
+    
+}
